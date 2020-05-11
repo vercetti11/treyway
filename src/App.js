806eea360efc0197b2d7a0615dfd72afde9048c2
Octavio <octavio@vercetti.vision>
2020-05-11 05:45:49 +0200

    Static Layout Set ✅

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const formPages = ["User", "Privacy", "Done"];
+  const [activePage, setActivePage] = useState(0);
+  const [progress, setProgress] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        {formPages.map((page, index) => (
+          <button
+            key={page}
+            disabled={progress < index}
+            onClick={() => setActivePage(index)}
+            className={page === formPages[activePage] ? "active" : ""}
+          >
+            {page}
+          </button>
+        ))}
+      </nav>
+      <form data-testid="form">
+        <div className="field">
+          <label htmlFor="name">
+            name:<span>*</span>
+          </label>
+          <input id="name" type="text" placeholder="John Smith" required />
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,10 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// TO Test
+// Show one page of the form at a time.
+// Show the current page position of the form.
+// Validate the input fields upon submission of each page.
+// If there are any validation errors, show an error message in the form and block progress.
+// Console.logs the resultant data on the final page.
